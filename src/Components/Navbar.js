011f61f7bcdc65134ac07969cd87cf329f6362b3
Kashish Goyal <Kashishgoyal961@gmail.com>
2022-03-25 00:49:48 +0530

    enabled\ dark mode

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,54 +1,54 @@
-import React, { useState } from 'react'
+import React from 'react'
 // importing proptypes(impt)
 import PropTypes from 'prop-types'
 
 export default function Navbar(props) {
-    const [myStyle, setMyStyle] = useState({
-        color: 'black',
-        backgroundColor: 'white',
-        borderColor: 'black'
-    })
+    // const [myStyle, setMyStyle] = useState({
+    //     color: 'black',
+    //     backgroundColor: 'white',
+    //     borderColor: 'black'
+    // })
 
-    const [btnText, setBtnText] = useState("Dark Mode")
+    // const [btnText, setBtnText] = useState("Dark Mode")
 
-    const toggleStyle = () => {
-        if (myStyle.color === 'black') {
-            setMyStyle({
-                color: 'white',
-                backgroundColor: 'black',
-                borderColor: 'white'
-            })
-            setBtnText("Light Mode");
-        } else {
-            setMyStyle({
-                color: 'black',
-                backgroundColor: 'white',
-                borderColor: 'black'
-            })
-            setBtnText("Dark Mode")
+    // const toggleStyle = () => {
+    //     if (myStyle.color === 'black') {
+    //         setMyStyle({
+    //             color: 'white',
+    //             backgroundColor: 'black',
+    //             borderColor: 'white'
+    //         })
+    //         setBtnText("Light Mode");
+    //     } else {
+    //         setMyStyle({
+    //             color: 'black',
+    //             backgroundColor: 'white',
+    //             borderColor: 'black'
+    //         })
+    //         setBtnText("Dark Mode")
 
-        }
-    }
+    //     }
+    // }
 
     return (
         <>
-            <nav className="navbar navbar-expand-lg" style={myStyle}>
-                <div className="container-fluid" style={myStyle}>
-                    <a className="navbar-brand" href="/" style={myStyle}>{props.title}</a>
+            <nav className={`navbar navbar-expand-lg navbar-${props.mode} bg-${props.mode}`} >
+                <div className="container-fluid" >
+                    <a className="navbar-brand" href="/" >{props.title}</a>
                     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon"></span>
                     </button>
                     <div className="collapse navbar-collapse" id="navbarSupportedContent">
                         <ul className="navbar-nav me-auto mb-2 mb-lg-0" >
                             <li className="nav-item">
-                                <a className="nav-link active" aria-current="page" href="/" style={myStyle}>Home</a>
+                                <a className="nav-link active" aria-current="page" href="/" >Home</a>
                             </li>
                             <li className="nav-item">
-                                <a className="nav-link active" href="/" style={myStyle}>{props.about}</a>
+                                <a className="nav-link active" href="/" >{props.about}</a>
                             </li>
                         </ul>
                         <div className="d-flex">
-                            <button className="btn btn-outline-success" style={myStyle} onClick={toggleStyle}>{btnText}</button>
+                            <button className="btn btn-outline-success" onClick={props.toggleMode}> {props.mode==='light'?'dark':'light'} Mode</button>
                         </div>
                     </div>
                 </div>
